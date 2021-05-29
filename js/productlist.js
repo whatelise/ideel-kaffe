a220b22b63d30a407baf0fd34c49790bd50b8344
Elise McCreary <aemccreary5@gmail.com>
2021-05-29 22:18:25 +0200

    dots working on productlist

--- js/productlist.js
+++ js/productlist.js
@@ -31,12 +31,12 @@ function showProducts(products) {
     copy.querySelector(".productImage").src = product.productimage;
     copy.querySelector(".productImage").alt = product.name + "product image";
 
-    const intensity = product.intensity;
-    const div = document.createElement("div") * intensity;
-    (div) => {
-      style.color = "#FF663E";
-      copy.querySelector(".rating").appendChild(div);
-    };
+    // const intensity = product.intensity;
+    // const div = document.createElement("div") * intensity;
+    // (div) => {
+    //   style.color = "#FF663E";
+    //   copy.querySelector(".rating").appendChild(div);
+    // };
 
     copy.querySelector(".price").textContent = product.price + ".00 DKK";
     // copy.querySelector(".shop-button").setAttribute("data-id", product._id);
@@ -47,6 +47,53 @@ function showProducts(products) {
     // copy.querySelector(
     //   ".viewProduct"
     // ).href = `productview.html?products=${product._id}`;
+    if (product.intensity == 5) {
+      console.log("product.intensity");
+      copy.querySelector("#intensity1").classList.add("circleFull");
+      copy.querySelector("#intensity1").classList.remove("circleEmpty");
+      copy.querySelector("#intensity2").classList.remove("circleEmpty");
+      copy.querySelector("#intensity2").classList.add("circleFull");
+      copy.querySelector("#intensity3").classList.remove("circleEmpty");
+      copy.querySelector("#intensity3").classList.add("circleFull");
+      copy.querySelector("#intensity4").classList.remove("circleEmpty");
+      copy.querySelector("#intensity4").classList.add("circleFull");
+      copy.querySelector("#intensity5").classList.remove("circleEmpty");
+      copy.querySelector("#intensity5").classList.add("circleFull");
+    }
+    if (product.intensity == 4) {
+      console.log("product.intensity");
+      copy.querySelector("#intensity1").classList.remove("circleEmpty");
+      copy.querySelector("#intensity1").classList.add("circleFull");
+      copy.querySelector("#intensity2").classList.remove("circleEmpty");
+      copy.querySelector("#intensity2").classList.add("circleFull");
+      copy.querySelector("#intensity3").classList.remove("circleEmpty");
+      copy.querySelector("#intensity3").classList.add("circleFull");
+      copy.querySelector("#intensity4").classList.remove("circleEmpty");
+      copy.querySelector("#intensity4").classList.add("circleFull");
+    }
+    if (product.intensity == 3) {
+      console.log("product.intensity");
+      copy.querySelector("#intensity1").classList.remove("circleEmpty");
+      copy.querySelector("#intensity1").classList.add("circleFull");
+      copy.querySelector("#intensity2").classList.remove("circleEmpty");
+      copy.querySelector("#intensity2").classList.add("circleFull");
+      copy.querySelector("#intensity3").classList.remove("circleEmpty");
+      copy.querySelector("#intensity3").classList.add("circleFull");
+    }
+
+    if (product.intensity == 2) {
+      console.log("product.intensity");
+      copy.querySelector("#intensity1").classList.remove("circleEmpty");
+      copy.querySelector("#intensity1").classList.add("circleFull");
+      copy.querySelector("#intensity2").classList.remove("circleEmpty");
+      copy.querySelector("#intensity2").classList.add("circleFull");
+    }
+
+    if (product.intensity == 1) {
+      copy.querySelector("#intensity1").classList.remove("circleEmpty");
+      copy.querySelector("#intensity1").classList.add("circleFull");
+    }
+
     document.querySelector(".product-list").appendChild(copy);
   });
 }
