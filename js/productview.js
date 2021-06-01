f37fd9268a572f2d93d8820c0fe78282d6c02a88
Adals-01 <anna355y@stud.kea.dk>
2021-06-01 14:27:47 +0200

    ugly cart  - kinda works

--- js/productview.js
+++ js/productview.js
@@ -29,10 +29,10 @@ function showProducts(product) {
   copy.querySelector(".origin2").textContent = product.origin;
   copy.querySelector(".process").textContent = product.process;
   copy.querySelector(".qgrade").textContent = product.qgrade + "/ 100";
-  // copy.querySelector(".shop-button").setAttribute("data-id", product._id);
-  // copy.querySelector(".shop-button").addEventListener("click", () => {
-  //   CART.add(product);
-  // });
+ copy.querySelector(".shop-button").setAttribute("data-id", product._id);
+copy.querySelector(".shop-button").addEventListener("click", () => {
+  CART.add(product);
+   });
 
   //*STRENGTH*//
   if (product.strength == 5) {
