221a79a7e774af820ae1d380529548de9f8f0e6a
Elise McCreary <aemccreary5@gmail.com>
2021-06-07 16:44:14 +0200

    testing testing link

--- js/index.js
+++ js/index.js
@@ -77,7 +77,7 @@ function showProducts(products) {
 
     copy.querySelector(
       ".viewProduct"
-    ).href = `ideel-kaffe/html/productview.html?products=${product._id}`;
+    ).href = `html/productview.html?products=${product._id}`;
 
     if (product.intensity == 5) {
       console.log("product.intensity");
